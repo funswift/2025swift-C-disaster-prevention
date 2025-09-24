b1b3f69c340b27fc02f39a2025d316ca54188411
Kanta Oikawa <iam@kantacky.com>
2025-09-24 15:52:03 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
 };
 
-export default nextConfig;
+const config: NextConfig = withPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+})(nextConfig);
+
+export default config;
